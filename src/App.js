7a8ab804a2a6884cbc33de97956a621ecc30375b
mrafiqkhan <mrafiq987@gmail.com>
2018-05-11 10:01:38 +0500

    made some progress in detection api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import './App.css';
 const particlesOptions={particles: { number:{value:30, density:{enable:true,value_area:800}}, line_linked: { shadow: {enable: true, color: "#3CA9D1", blur: 5}}}};
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+    }
+  }
+
+  onInputChange = (event)=>{
+    console.log(event.target.value);
+  }
   render() {
     return (
       <div className="App">
@@ -16,7 +26,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
       {
         /*
       <FaceRecognition />} */
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './imageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange}) => {
   return (
     <div>
       <p className='f3'>{'This Magic Brain will detect face in your pictures. Get it a try'}</p>
       <div className='center'>
         <div className='form center pa4 br3 w-70'>
-          <input type='text' className='f4 pa2 w-70 center' placeholder='Select and Image' />
+          <input type='text' className='f4 pa2 w-70 center' placeholder='Select and Image' onChange={onInputChange} />
           <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
         </div>
       </div>
