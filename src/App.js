d07c4a2e134fb7b5e7249657a07a62b7f726389e
mrafiqkhan <mrafiq987@gmail.com>
2018-05-18 19:11:43 +0500

    made some changes in registration form

--- src/App.js
+++ src/App.js
@@ -64,15 +64,19 @@ displayFaceBox = (box) =>{
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
         <Navigation onRouteChange={this.onRouteChange} />
-        {this.state.route ==='signin' 
-          ?<Signin  onRouteChange={this.onRouteChange} />
-          :
-          <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
-          </div>
+        {this.state.route ==='home' 
+          ? <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        </div>
+          
+          :(this.state.route==='signin'
+          ? <Signin  onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange} />) 
+          
+         
         }
       </div>
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -8,6 +8,10 @@ const Register = ({onRouteChange}) => {
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
       <legend className="f1 fw6 ph0 mh0">{'Sign In'}</legend>
       <div className="mt3">
+        <label className="db fw6 lh-copy f6" for="name">{'Name'}</label>
+        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+      </div>
+      <div className="mt3">
         <label className="db fw6 lh-copy f6" for="email-address">{'Email'}</label>
         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
       </div>
@@ -24,9 +28,6 @@ const Register = ({onRouteChange}) => {
       type="submit" 
       value="Sign in" 
       />
-    </div>
-    <div className="lh-copy mt3">
-      <a href="" className="f6 link dim black db">{'Register'}</a>
     </div>
   </form>
   </article>
