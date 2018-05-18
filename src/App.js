2965b8538fb394a8769aae3522d19f876b8c624b
mrafiqkhan <mrafiq987@gmail.com>
2018-05-18 13:13:39 +0500

    completed face detecting process

--- src/App.js
+++ src/App.js
@@ -32,13 +32,24 @@ class App extends Component {
     const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
-     console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow:height - (clarifaiFace.bottom_row * height)
+
+    }
+}
+
+displayFaceBox = (box) =>{
+  console.log(box);
+  this.setState({box:box});
 }
   onButtonSubmit = ()=>{
     this.setState({imageUrl:this.state.input});
     //output response
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.calculateFaceLocation(response))
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err));
   }
   render() {
@@ -49,7 +60,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
 
     );
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './faceRecognation.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
   return (
     <div className='center imageBox'>
-    <div className='innerDiv'>
+    <div className='innerDiv absolute mt2'>
      <img id="inputImage" alt='' src={imageUrl} />
+     <div className='bounding-box' style={{top:box.topRow, right:box.rightCol, bottom:box.bottomRow, left:box.leftCol}}></div>
      </div>
     </div>
   );
