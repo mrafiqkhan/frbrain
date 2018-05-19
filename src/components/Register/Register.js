7c27c74711176b8d682c53219c15daa72bb58eba
mrafiqkhan <mrafiq987@gmail.com>
2018-05-19 16:03:44 +0500

    started working on register js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,7 +6,7 @@ const Register = ({onRouteChange}) => {
         <article class="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l shadow-5 mw6 center">
         <form className="measure">
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-      <legend className="f1 fw6 ph0 mh0">{'Sign In'}</legend>
+      <legend className="f1 fw6 ph0 mh0">{'Register'}</legend>
       <div className="mt3">
         <label className="db fw6 lh-copy f6" for="name">{'Name'}</label>
         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
@@ -26,7 +26,7 @@ const Register = ({onRouteChange}) => {
       onClick={() => onRouteChange('home')}
       className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
       type="submit" 
-      value="Sign in" 
+      value="Register" 
       />
     </div>
   </form>
